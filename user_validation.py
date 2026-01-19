8666b58330fb81e93e6517378c8675e2f76ad3b1
Alejandro Gutierrez <99373353+AlejandroGutierrezArcila@users.noreply.github.com>
2026-01-19 13:45:23 -0500

    Add user validation example

---
+++ user_validation.py
@@ -0,0 +1,9 @@
+# User validation example
+
+username = input("Ingrese Usuario: ")
+password = input("Ingrese Contraseña: ")
+
+if username == "admin" and password == "1234":
+    print("Acceso Concedido")
+else:
+    print("Acceso Denegado")
